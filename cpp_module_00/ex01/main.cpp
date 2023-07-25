71f230f5aa64166c3a3b321cfc961f96d8b1a9c6
Muhammet ali Yazici <muyazici@k2m12s09.42kocaeli.com.tr>
2023-07-25 17:06:47 +0300

    private getter setter ex01

--- cpp_module_00/ex01/main.cpp
+++ cpp_module_00/ex01/main.cpp
@@ -1,25 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muyazici <muyazici@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/25 13:45:07 by muyazici          #+#    #+#             */
+/*   Updated: 2023/07/25 16:36:41 by muyazici         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "phonebook.hpp"
 
 void	Add(PhoneBook *pb)
 {
-	pb->sayac = pb->sayac % 8;
+	std::string input;
+	pb->counter = pb->counter % 8;
     if(pb->filled < 8) {
         pb->filled++;
     }
     std::cout << "\033[1;92mPerson Informations\033[0m" << std::endl;
     std::cout << "Name: ";
-    std::getline(std::cin, pb->contacts[pb->sayac].name);
+    std::getline(std::cin, input);
+	pb->contacts[pb->counter].setName(input);
     std::cout << "Surname: ";
-    std::getline(std::cin, pb->contacts[pb->sayac].surName);
+    std::getline(std::cin, input);
+	pb->contacts[pb->counter].setsurName(input);
     std::cout << "Nickname: ";
-    std::getline(std::cin, pb->contacts[pb->sayac].nickName);
+    std::getline(std::cin, input);
+	pb->contacts[pb->counter].setnickName(input);
     std::cout << "Phone Number: ";
-    std::getline(std::cin, pb->contacts[pb->sayac].num);
+    std::getline(std::cin, input);
+	pb->contacts[pb->counter].setnum(input);
     std::cout << "Secret: ";
-    std::getline(std::cin, pb->contacts[pb->sayac].secret);
-    if (pb->contacts[pb->sayac].name.empty() || pb->contacts[pb->sayac].surName.empty() || pb->contacts[pb->sayac].nickName.empty())
-        return ;
-    pb->sayac++;
+    std::getline(std::cin, input);
+	pb->contacts[pb->counter].setsecret(input);
+    if (pb->contacts[pb->counter].getName().empty() || pb->contacts[pb->counter].getsurName().empty() || pb->contacts[pb->counter].getnickName().empty())
+	{
+		return ;
+	}
+    pb->counter++;
 }
 
 void	Search(PhoneBook *pb)
@@ -42,9 +62,9 @@ void	Search(PhoneBook *pb)
 	{
 		std::cout << separator
             << "| " << std::right << std::setw(10) << i + 1
-            << "| " << std::right << std::setw(10) << (pb->contacts[i].name.length() > 10 ? pb->contacts[i].name.substr(0,9) + '.' : pb->contacts[i].name)
-            << "| " << std::right << std::setw(10) << (pb->contacts[i].surName.length() > 10 ? pb->contacts[i].surName.substr(0,9) + '.' : pb->contacts[i].surName)
-            << "| " << std::right << std::setw(10) << (pb->contacts[i].nickName.length() > 10 ? pb->contacts[i].nickName.substr(0,9) + '.' : pb->contacts[i].nickName)
+            << "| " << std::right << std::setw(10) << (pb->contacts[i].getName().length() > 10 ? pb->contacts[i].getName().substr(0,9) + '.' : pb->contacts[i].getName())
+            << "| " << std::right << std::setw(10) << (pb->contacts[i].getsurName().length() > 10 ? pb->contacts[i].getsurName().substr(0,9) + '.' : pb->contacts[i].getsurName())
+            << "| " << std::right << std::setw(10) << (pb->contacts[i].getnickName().length() > 10 ? pb->contacts[i].getnickName().substr(0,9) + '.' : pb->contacts[i].getnickName())
             << "| " << std::endl << separator;
             i++;
 	}
@@ -58,11 +78,11 @@ void	Search(PhoneBook *pb)
 	if (controller <= pb->filled && controller >= 1)
 	{
 		std::cout << "\033[1;95mInformation -> \033[0m" << controller <<std::endl;
-		std::cout << "Name: " << pb->contacts[controller - 1].name << std::endl;
-		std::cout << "SurName: " << pb->contacts[controller - 1].surName << std::endl;
-		std::cout << "Nickname: " << pb->contacts[controller - 1].nickName << std::endl;
-		std::cout << "TelNumber: " << pb->contacts[controller - 1].num << std::endl;
-		std::cout << "DarkestSecret: " << pb->contacts[controller - 1].secret << std::endl;
+		std::cout << "Name: " << pb->contacts[controller - 1].getName() << std::endl;
+		std::cout << "SurName: " << pb->contacts[controller - 1].getsurName() << std::endl;
+		std::cout << "Nickname: " << pb->contacts[controller - 1].getnickName() << std::endl;
+		std::cout << "TelNumber: " << pb->contacts[controller - 1].getnum() << std::endl;
+		std::cout << "DarkestSecret: " << pb->contacts[controller - 1].getsecret() << std::endl;
 		std::cout << "\033[1;95m>-------------------------< \033[0m" << std::endl;
 	}
 	else
@@ -75,7 +95,7 @@ int	main()
 	PhoneBook pb;
 	std::string str;
 
-	pb.sayac = 0;
+	pb.counter = 0;
 	pb.filled = 0;
 	std::cout << "Enter your command -> ADD, SEARCH, EXIT" << std::endl;
 	std::cout << "Make your choice : ";
--- cpp_module_00/ex01/phonebook.hpp
+++ cpp_module_00/ex01/phonebook.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muyazici <muyazici@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/25 13:45:15 by muyazici          #+#    #+#             */
+/*   Updated: 2023/07/25 16:21:07 by muyazici         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHONEBOOK_HPP
 #define PHONEBOOK_HPP
 
@@ -6,23 +18,33 @@
 
 class Contact
 {
-public:
+private:
 	std::string name;
 	std::string surName;
 	std::string nickName;
 	std::string	secret;
 	std::string num;
 
+public:
 	Contact(){};
-
 	~Contact(){};
+	void setName(std::string gname){name = gname;}
+	void setsurName(std::string gsurName){surName = gsurName;}
+	void setnickName(std::string gnickName){nickName = gnickName;}
+	void setsecret(std::string gsecret){secret = gsecret;}
+	void setnum(std::string gnum){num = gnum;}
+	std::string getName(){return name;}
+	std::string getsurName(){return surName;}
+	std::string getnickName(){return nickName;}
+	std::string getsecret(){return secret;}
+	std::string getnum(){return num;}
 };
 
 class PhoneBook
 {
 public:
 	Contact contacts[8];
-	int sayac;
+	int counter;
 	int filled;
 
 	PhoneBook(){};
